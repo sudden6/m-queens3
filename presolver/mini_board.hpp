215759008b14b0ec0c1b38e6a146e55c1fbfbe5e
sudden6 <sudden6@users.noreply.github.com>
2023-05-20 14:42:24 +0200

    Reduce storage requirements even more

--- presolver/mini_board.hpp
+++ presolver/mini_board.hpp
@@ -8,14 +8,15 @@ namespace queens {
  * Minimal storage version of board.hpp
  */
 class mini_board {
-        // TODO: might be possible to store bv and bh with fewer bits
-        uint64_t m_bv;
-        uint64_t m_bh;
         uint64_t m_bu;
         uint64_t m_bd;
+        uint32_t m_bv;
+        uint32_t m_bh;
 
     public:
-        mini_board(Board brd) : m_bv{brd.getBV()}, m_bh{brd.getBH()}, m_bu{brd.getBU()}, m_bd{brd.getBD()} {}
+        mini_board(Board brd)
+            : m_bu{brd.getBU()}, m_bd{brd.getBD()}, m_bv{static_cast<uint32_t>(brd.getBV())},
+              m_bh{static_cast<uint32_t>(brd.getBH())} {}
 
         uint64_t getBV() const { return m_bv; }
         uint64_t getBH() const { return m_bh; }
