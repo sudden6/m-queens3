c55de7ff5aaf72645339b1b41c787c013d18a032
sudden6 <sudden6@users.noreply.github.com>
2023-05-21 13:50:50 +0200

    Validate boards when packing

--- presolver/mini_board.hpp
+++ presolver/mini_board.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #include "board.hpp"
+#include <bitset>
 #include <cstdint>
+#include <numeric>
+
+#include "../bithacks.hpp"
 
 namespace queens {
 /**
@@ -16,11 +20,19 @@ class mini_board {
     public:
         mini_board(Board brd)
             : m_bu{brd.getBU()}, m_bd{brd.getBD()}, m_bv{static_cast<uint32_t>(brd.getBV())},
-              m_bh{static_cast<uint32_t>(brd.getBH())} {}
+              m_bh{static_cast<uint32_t>(brd.getBH())} {
+            assert(valid(brd.placed));
+        }
 
         uint64_t getBV() const { return m_bv; }
         uint64_t getBH() const { return m_bh; }
         uint64_t getBU() const { return m_bu; }
         uint64_t getBD() const { return m_bd; }
+
+    private:
+        bool valid(uint8_t placed) {
+            return bithacks::count_bits_set(m_bu) == placed && bithacks::count_bits_set(m_bd) == placed &&
+                   bithacks::count_bits_set(m_bv) == placed && bithacks::count_bits_set(m_bh) == placed;
+        }
 };
 } // namespace queens
